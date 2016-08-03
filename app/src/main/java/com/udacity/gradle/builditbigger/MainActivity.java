8776fbf12ce92ea409b5c8b0f625d9762536ca4e
Garrison Wright <wrightnow99@gmail.com>
2016-08-02 19:41:14 -0500

    Baseline

---
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -0,0 +1,63 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.Joker;
+
+import garrison.com.jokerapp.JokerMainActivity;
+
+public class MainActivity extends ActionBarActivity {
+
+    public static final String JOKE_STRING = "joke_string";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void tellJoke(View view){
+        Joker joker = new Joker();
+        String joke  = joker.getJoke();
+        Toast.makeText(this, joke, Toast.LENGTH_SHORT).show();
+    }
+
+    public void launchJokeApp(View view) {
+        Intent intent = new Intent(this, JokerMainActivity.class);
+        intent.putExtra(JOKE_STRING, "My Joke is Funnier");
+        startActivity(intent);
+
+    }
+
+}
