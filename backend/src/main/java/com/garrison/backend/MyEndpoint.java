b63a849c456f73a5402cf0e603363e9f4f5e64f4
Garrison Wright <wrightnow99@gmail.com>
2016-08-15 20:27:20 -0500

    Changed to actually return a joke instead of a name.

--- backend/src/main/java/com/garrison/backend/MyEndpoint.java
+++ backend/src/main/java/com/garrison/backend/MyEndpoint.java
@@ -26,9 +26,10 @@ public class MyEndpoint {
 
     /** A simple endpoint method that takes a name and says Hi back */
     @ApiMethod(name = "sayHi")
-    public MyBean sayHi(@Named("name") String name) {
+    //public MyBean sayHi(@Named("name") String name) {
+    public MyBean sayHi() {
         MyBean response = new MyBean();
-        response.setData("Hi, " + name);
+        response.setData("Five-fourths of people admit that they are bad with fractions.");
 
         return response;
     }
