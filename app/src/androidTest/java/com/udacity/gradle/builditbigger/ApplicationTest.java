6c37f9eb0d2c3f77e0761f0a4a5f7d8a4c209c5a
Garrison Wright <wrightnow99@gmail.com>
2016-09-15 07:47:04 -0500

    Application Test added

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
@@ -1,13 +1,39 @@
 package com.udacity.gradle.builditbigger;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.content.Context;
+import android.util.Pair;
+import android.test.AndroidTestCase;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+
+
+import java.util.concurrent.ExecutionException;
+
+public class ApplicationTest extends AndroidTestCase {
+
+    //public static final String TAG = AsyncTest.class.getName();
+    public void testAsynchNotBlankResponse() {
+        String result = "";
+
+        EndpointsAsyncTask endpointsAsyncTask = (EndpointsAsyncTask) new EndpointsAsyncTask().execute(new Pair<Context, String>(getContext(), null));
+
+        try {
+            result = endpointsAsyncTask.get();
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        if (result != null && result.trim().length() > 0) {
+            assert (true);
+        } else {
+            assert (false);
+        }
+
     }
 }
